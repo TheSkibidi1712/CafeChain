d15f0e289f195ca676c00ae601d26b7e0d407b2d
the <anhnttb001711@gmail.com>
2026-07-23 12:28:11 +0700

    Merge Feature Kho

--- CafeChain/Migrations/20260723052417_InitialCreate.Designer.cs
+++ CafeChain/Migrations/20260723052417_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CafeChain.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260722175057_InitialCreate")]
+    [Migration("20260723052417_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
