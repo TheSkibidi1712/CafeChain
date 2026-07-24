7dca0f77483ac3da1f3b39ee13200704ce67facf
the <anhnttb001711@gmail.com>
2026-07-24 13:51:04 +0700

    Merge Feature POS

--- CafeChain/Migrations/20260724064847_InitialCreate.Designer.cs
+++ CafeChain/Migrations/20260724064847_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CafeChain.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260723052417_InitialCreate")]
+    [Migration("20260724064847_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
